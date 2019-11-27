32a0ed2d5e675ddf25f0d18428d821da34842a9e
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-27 17:41:36 -0300

    Aula 05

--- App.js
+++ App.js
@@ -6,7 +6,24 @@ import LoginScreen from './src/pages/LoginScreen';
 const AppNavigator = createStackNavigator({
   Login: {
     screen: LoginScreen,
+    navigationOptions: {
+      title: 'Bem vindo',
+    }
   },
+}, {
+  defaultNavigationOptions: {
+    title: "Minhas series",
+    hearderTintColor: 'white',
+    headerStyle: {
+      backgroundColor: '#003994',
+      borderBottomWidth: 1,
+      borderBottomColor: '#c5c5c5',
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontSize: 30,
+    }
+  }
 });
 
 const AppContainer = createAppContainer(AppNavigator);
