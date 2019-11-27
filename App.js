f3c13bf446798944b22a8e064fe3e4d5e38bff17
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-27 13:38:10 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import LoginScreen from './src/pages/LoginScreen';
+
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: LoginScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
