bab0c670679d6b2d7676cc6eeecbd25282d2f412
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-28 01:58:13 -0300

    Aula 22

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Button,
   ActivityIndicator,
+  Alert,
 } from 'react-native';
 import * as firebase from 'firebase';
 
@@ -51,26 +52,62 @@ export default class LoginScreen extends React.Component {
     this.setState({isLoading: true});
     const {email, password} = this.state;
 
+    const loginUserSuccess = user => {
+      this.setState({message: 'Sucesso!'});
+    };
+
+    const loginUserFailed = error => {
+      this.setState({message: this.getMessageByError(error.code)});
+    };
+
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(user => this.setState({ message: 'Login realizado com sucesso!'}))
-      .catch(error => this.setState({ message: this.getMessageByError(error.code)}))
+      .then(loginUserSuccess)
+      .catch(error => {
+        if (error.code == 'auth/user-not-found') {
+          Alert.alert(
+            'Usuário não encontrado',
+            'Deseja criar um novo usuário?',
+            [
+              {
+                text: 'Não',
+                onPress: () => {
+                  console.log('não quis');
+                },
+              },
+              {
+                text: 'Sim',
+                onPress: () => {
+                  firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then(loginUserSuccess)
+                    .catch(loginUserFailed);
+                },
+              },
+            ],
+            {cancelable: false},
+          );
+        } else {
+          loginUserFailed(error);
+        }
+      })
       .then(() => this.setState({isLoading: false}));
   }
 
   getMessageByError(code) {
     switch (code) {
-      case "auth/user-not-found":
-        return "E-mail inexistente";
+      case 'auth/user-not-found':
+        return 'E-mail inexistente';
         break;
-      case "auth/wrong-password":
-        return "Senha incorreta";
-      case "auth/invalid-email":
-        return "Digite um e-mail válido";
+      case 'auth/wrong-password':
+        return 'Senha incorreta';
+      case 'auth/invalid-email':
+        return 'Digite um e-mail válido';
         break;
       default:
-        return "Erro desconhecido";
+        return 'Erro desconhecido';
         break;
     }
   }
