c734ba55385fe93d251229ce43dc6e0af9b08435
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-28 11:33:21 -0300

    Aula 45

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,59 +1,53 @@
 import * as firebase from 'firebase';
 import {Alert} from 'react-native';
 
-const USER_LOGIN = 'USER_LOGIN';
-const userLogin = user => ({
-  type: USER_LOGIN,
+export const USER_LOGIN_SUCESS = 'USER_LOGIN';
+const userLoginSucess = user => ({
+  type: USER_LOGIN_SUCESS,
   user,
 });
 
-const USER_LOGOUT = 'USER_LOGOUT';
+export const USER_LOGOUT = 'USER_LOGOUT';
 const userLogout = () => ({
   type: USER_LOGOUT,
 });
 
-export const processLogin = ({email, password}) => {
-  const loginUserSuccess = user => {
-    this.setState({message: 'Sucesso!'});
-    this.props.navigation.navigate('Main');
-  };
-
-  const loginUserFailed = error => {
-    this.setState({message: this.getMessageByError(error.code)});
-  };
-
+export const processLogin = ({email, password}) => dispatch => {
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
-    .then(loginUserSuccess)
-    .catch(error => {
-      if (error.code == 'auth/user-not-found') {
-        Alert.alert(
-          'Usuário não encontrado',
-          'Deseja criar um novo usuário?',
-          [
-            {
-              text: 'Não',
-              onPress: () => {
-                console.log('não quis');
-              },
-            },
-            {
-              text: 'Sim',
-              onPress: () => {
-                firebase
-                  .auth()
-                  .createUserWithEmailAndPassword(email, password)
-                  .then(loginUserSuccess)
-                  .catch(loginUserFailed);
-              },
-            },
-          ],
-          {cancelable: false},
-        );
-      } else {
-        loginUserFailed(error);
-      }
-    })
-    .then(() => this.setState({isLoading: false}));
+    .then((user) => {
+      const action = userLoginSucess(user);
+      dispatch(action);
+    });
+    // .catch(error => {
+    //   if (error.code == 'auth/user-not-found') {
+    //     Alert.alert(
+    //       'Usuário não encontrado',
+    //       'Deseja criar um novo usuário?',
+    //       [
+    //         {
+    //           text: 'Não',
+    //           onPress: () => {
+    //             console.log('não quis');
+    //           },
+    //         },
+    //         {
+    //           text: 'Sim',
+    //           onPress: () => {
+    //             firebase
+    //               .auth()
+    //               .createUserWithEmailAndPassword(email, password)
+    //               .then(loginUserSuccess)
+    //               .catch(loginUserFailed);
+    //           },
+    //         },
+    //       ],
+    //       {cancelable: false},
+    //     );
+    //   } else {
+    //     loginUserFailed(error);
+    //   }
+    // })
+    // .then(() => this.setState({isLoading: false}));
 };
