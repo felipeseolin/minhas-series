621eb2a5eb80000bc67bc7ee9f2f8b9d26aa852f
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-28 11:24:01 -0300

    Aula 44

--- src/SeriesApp.js
+++ src/SeriesApp.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
-import devToolsEnhancer from 'remote-redux-devtools';
+import {composeWithDevTools} from 'remote-redux-devtools';
 
 import Router from './Router';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxThunk)),
+);
 
 const SeriesApp = prop => (
   <Provider store={store}>
