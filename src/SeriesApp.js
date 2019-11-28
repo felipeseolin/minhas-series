5c80ea362f30f40f7aca5f6b72aecd847ff4d3eb
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-28 10:45:38 -0300

    Aula 38

--- src/SeriesApp.js
+++ src/SeriesApp.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import Router from './Router';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
-const SeriesApp = prop => <Router />;
+import Router from './Router';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
+const SeriesApp = prop => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
 
 export default SeriesApp;
