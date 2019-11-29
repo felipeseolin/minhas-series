bc07f70e3abfa920030b50eccee9ffde9ac0de28
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-29 13:21:22 -0300

    Aula 103

--- src/pages/NewSerieScreen.js
+++ src/pages/NewSerieScreen.js
@@ -7,6 +7,7 @@ import {
   Picker,
   ScrollView,
   Button,
+  ActivityIndicator,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {setField, saveSerie} from '../actions';
@@ -14,67 +15,91 @@ import Slider from '@react-native-community/slider';
 
 import FormRow from '../components/FormRow';
 
-const NewSerieScreen = ({serieForm, setField, saveSerie, navigation}) => (
-  <ScrollView>
-    <FormRow>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Título"
-        value={serieForm.title}
-        onChangeText={value => setField('title', value)}
-      />
-    </FormRow>
-    <FormRow>
-      <TextInput
-        style={styles.textInput}
-        placeholder="URL da imagem"
-        value={serieForm.img}
-        onChangeText={value => setField('img', value)}
-      />
-    </FormRow>
+class NewSerieScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <FormRow>
-      <Picker
-        selectedValue={serieForm.gender}
-        onValueChange={item => setField('gender', item)}>
-        <Picker.Item label="Ação" value="action" />
-        <Picker.Item label="Comédia" value="comedy" />
-        <Picker.Item label="Drama" value="drama" />
-        <Picker.Item label="Infantil" value="kids" />
-      </Picker>
-    </FormRow>
-    <FormRow>
-      <View style={styles.rate}>
-        <Text>Nota: </Text>
-        <Text>{serieForm.rate}</Text>
-      </View>
-      <Slider
-        minimumValue={0}
-        maximumValue={100}
-        step={5}
-        value={serieForm.rate}
-        onValueChange={value => setField('rate', value)}
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  render() {
+    const {serieForm, setField, saveSerie, navigation} = this.props;
+    return (
+      <ScrollView>
+        <FormRow>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Título"
+            value={serieForm.title}
+            onChangeText={value => setField('title', value)}
+          />
+        </FormRow>
+        <FormRow>
+          <TextInput
+            style={styles.textInput}
+            placeholder="URL da imagem"
+            value={serieForm.img}
+            onChangeText={value => setField('img', value)}
+          />
+        </FormRow>
+
+        <FormRow>
+          <Picker
+            selectedValue={serieForm.gender}
+            onValueChange={item => setField('gender', item)}>
+            <Picker.Item label="Ação" value="action" />
+            <Picker.Item label="Comédia" value="comedy" />
+            <Picker.Item label="Drama" value="drama" />
+            <Picker.Item label="Infantil" value="kids" />
+          </Picker>
+        </FormRow>
+        <FormRow>
+          <View style={styles.rate}>
+            <Text>Nota: </Text>
+            <Text>{serieForm.rate}</Text>
+          </View>
+          <Slider
+            minimumValue={0}
+            maximumValue={100}
+            step={5}
+            value={serieForm.rate}
+            onValueChange={value => setField('rate', value)}
+          />
+        </FormRow>
+        <FormRow>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Descrição"
+            value={serieForm.description}
+            onChangeText={value => setField('description', value)}
+            numberOfLines={5}
+            multiline={true}
+          />
+        </FormRow>
+
+        {this.renderButton()}
+      </ScrollView>
+    );
+  }
+
+  renderButton() {
+    if (this.state.isLoading) {
+      return <ActivityIndicator />;
+    }
+
+    return (
+      <Button
+        title="Salvar"
+        onPress={async () => {
+          await saveSerie(serieForm);
+          navigation.goBack();
+        }}
       />
-    </FormRow>
-    <FormRow>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Descrição"
-        value={serieForm.description}
-        onChangeText={value => setField('description', value)}
-        numberOfLines={5}
-        multiline={true}
-      />
-    </FormRow>
-    <Button
-      title="Salvar"
-      onPress={async () => {
-        await saveSerie(serieForm);
-        navigation.goBack();
-      }}
-    />
-  </ScrollView>
-);
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   textInput: {
