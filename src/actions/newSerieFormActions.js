1758be5d2e3dbe3b8c2591a84be658b47956f03e
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-29 12:43:22 -0300

    Aula 96

--- src/actions/newSerieFormActions.js
+++ src/actions/newSerieFormActions.js
@@ -1,3 +1,5 @@
+import * as firebase from 'firebase';
+
 export const SET_FIELD = 'SET_FIELD';
 
 export const setField = (field, value) => {
@@ -9,5 +11,10 @@ export const setField = (field, value) => {
 };
 
 export const saveSerie = serie => {
+  const {currentUser} = firebase.auth();
+  const db = firebase
+    .database()
+    .ref(`/users/${currentUser.displayName}/series`);
+
   console.log('serie que vai ser salva', serie);
 };
