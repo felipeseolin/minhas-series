08625b26c1e016168f1ab6e5aac3de994d98d7f0
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-29 00:22:13 -0300

    Correção

--- src/pages/SerieDetailScreen.js
+++ src/pages/SerieDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScroolView, View, Text, Image, StyleSheet} from 'react-native';
+import {ScrollView, View, Text, Image, StyleSheet} from 'react-native';
 import Line from '../components/Line';
 import LongText from '../components/LongText';
 
@@ -8,13 +8,13 @@ export default class SerieDetailScreen extends React.Component {
     const {serie} = this.props.navigation.state.params;
 
     return (
-      <View>
+      <ScrollView>
         <Image source={{uri: serie.img}} style={styles.image} />
         <Line label="Título" content={serie.title} />
         <Line label="Gênero" content={serie.gender} />
         <Line label="Nota" content={serie.rate} />
         <LongText label="Descrição" content={serie.description} />
-      </View>
+      </ScrollView>
     );
   }
 }
